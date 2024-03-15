1c869a0c4dfbcb1afaaa048fe4fe3290fdbee00f
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-03-15 23:36:25 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-10/12 mx-auto max-w-[1440px] font-lexend">
+      <h1 className="text-3xl font-bold underline text-primary-green">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-green": "#0BE58A",
+        "dark-full": "#150B2B",
+        "dark-two": "#282828",
+        "dark-six": "#878787",
+        "dark-80": "#282828CC",
+        "dark-70": "#150B2BB3",
+        "dark-60 ": "#150B2B99",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
